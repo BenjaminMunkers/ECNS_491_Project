b08ceda82ad6702eedbf059be11cf92276a136de
Kade Aldrich <kadealdrich@gmail.com>
2022-11-09 23:24:01 -0700

    create exploratory script

---
+++ exploratory.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(readxl)
+library(ggplo2)
+
+# changing the wd to knit the document
+# comment out as needed
+wd2 = "C:/Fall 22/ECNS 491/course_proj/ECNS_491_Project/Data"
+setwd(wd2)
+
+# loading in final cleaned dataframe
+load("finalcleaned.RData")
+
