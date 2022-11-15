07458a11fc0c3e76df4ee07a4683506beed073dc
Kade Aldrich <kadealdrich@gmail.com>
2022-11-14 23:42:11 -0700

    set wd in main

--- scripts/main.R
+++ scripts/main.R
@@ -1,5 +1,6 @@
 ### Main Script to run other scripts from
-
+# set wd to scripts folder
+setwd(paste(getwd(), "/scripts", sep = ""))
 source("datawrangling.R")
 
 wd = str_replace(getwd(), "Data", "scripts")
